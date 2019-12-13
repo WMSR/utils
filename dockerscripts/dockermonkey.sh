db107b556848920807c42ec650ae871ec926b6d0
harold-ogden-walmart <harold.ogden@walmart.com>
2019-12-13 12:39:03 -0600

    starting soffice.bin prior to vipermonkey run to deal with a weird process backgrounding issue

--- dockerscripts/dockermonkey.sh
+++ dockerscripts/dockermonkey.sh
@@ -3,6 +3,7 @@ if [[ $(sudo docker ps -f status=running -f ancestor=haroldogden/vipermonkey -l 
 	echo "[+] Docker container is already running!"
 else
 	echo "[!] Container is not running. Starting now..."
+	sudo docker pull haroldogden/vipermonkey:latest
 	sudo docker run --rm -d -t haroldogden/vipermonkey:latest
 fi
 
@@ -12,6 +13,10 @@ echo "[*] Attempting to copy file $1 into docker ID $docker_id"
 
 file_basename=$(basename $1)
 
+echo "[*] Starting openoffice listener for file content conversions..."
+
+sudo docker exec $docker_id sh -c '/usr/lib/libreoffice/program/soffice.bin --headless --invisible --nocrashreport --nodefault --nofirststartwizard --nologo --norestore --accept="socket,host=127.0.0.1,port=2002,tcpNoDelay=1;urp;StarOffice.ComponentContext" &'
+
 echo "[*] Checking for ViperMonkey updates..."
 
 sudo docker exec $docker_id sh -c "cd /opt;for d in *; do cd \$d; git pull; cd /opt; done"
@@ -19,3 +24,5 @@ sudo docker exec $docker_id sh -c "cd /opt;for d in *; do cd \$d; git pull; cd /
 sudo docker cp $1 $docker_id:/root/$file_basename
 sudo docker exec $docker_id sh -c "/opt/ViperMonkey/vipermonkey/vmonkey.py -s --ioc /root/$file_basename"
 
+echo "[*] Done - Killing docker container $docker_id"
+sudo docker stop $docker_id > /dev/null
