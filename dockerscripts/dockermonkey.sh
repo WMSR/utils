71e54442e6545efbfb8b9bb9818aa098f22b179d
harold-ogden-walmart <harold.ogden@walmart.com>
2019-12-19 08:04:28 -0600

    removed sudo in script, added check to make sure docker runs before trying to create container

--- dockerscripts/dockermonkey.sh
+++ dockerscripts/dockermonkey.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-if [[ $(sudo docker ps -f status=running -f ancestor=haroldogden/vipermonkey -l | tail -n +2) ]]; then
+echo "[*] Running 'docker ps' to see if script has required privileges to run..."
+
+docker ps
+
+if [ $? -ne 0 ]; then
+	echo "[!] 'docker ps' failed to run - you may not have the privileges to run docker. Try using sudo."
+	exit
+fi
+
+if [[ $(docker ps -f status=running -f ancestor=haroldogden/vipermonkey -l | tail -n +2) ]]; then
         echo "[+] Docker container is already running!"
 else
         echo "[!] Container is not running. Starting now..."
-        sudo docker pull haroldogden/vipermonkey:latest
-        sudo docker run --rm -d -t haroldogden/vipermonkey:latest
+        docker pull haroldogden/vipermonkey:latest
+        docker run --rm -d -t haroldogden/vipermonkey:latest
 fi
 
-docker_id=$(sudo docker ps -f status=running -f ancestor=haroldogden/vipermonkey -l | tail -n +2 | cut -f1 -d' ')
+docker_id=$(docker ps -f status=running -f ancestor=haroldogden/vipermonkey -l | tail -n +2 | cut -f1 -d' ')
 
 echo "[*] Attempting to copy file $1 into container ID $docker_id"
 
@@ -15,29 +24,29 @@ file_basename=$(basename "$1")
 
 echo "[*] Starting openoffice listener for file content conversions..."
 
-sudo docker exec $docker_id sh -c '/usr/lib/libreoffice/program/soffice.bin --headless --invisible --nocrashreport --nodefault --nofirststartwizard --nologo --norestore --accept="socket,host=127.0.0.1,port=2002,tcpNoDelay=1;urp;StarOffice.ComponentContext" &'
+docker exec $docker_id sh -c '/usr/lib/libreoffice/program/soffice.bin --headless --invisible --nocrashreport --nodefault --nofirststartwizard --nologo --norestore --accept="socket,host=127.0.0.1,port=2002,tcpNoDelay=1;urp;StarOffice.ComponentContext" &'
 
 echo "[*] Checking for ViperMonkey and dependency updates..."
 
-sudo docker exec $docker_id sh -c "cd /opt;for d in *; do cd \$d; git pull; cd /opt; done"
+docker exec $docker_id sh -c "cd /opt;for d in *; do cd \$d; git pull; cd /opt; done"
 
 echo "[*] Disabling network connection for container ID $docker_id"
-sudo docker network disconnect bridge $docker_id
+docker network disconnect bridge $docker_id
 
-sudo docker cp "$1" "$docker_id:/root/$file_basename"
+docker cp "$1" "$docker_id:/root/$file_basename"
 
 if [ $# -eq 1 ]
   then
-sudo docker exec $docker_id sh -c "/opt/ViperMonkey/vipermonkey/vmonkey.py -s --ioc '/root/$file_basename'"
+docker exec $docker_id sh -c "/opt/ViperMonkey/vipermonkey/vmonkey.py -s --ioc '/root/$file_basename'"
 elif [ $# -eq 2 ]
   then
-sudo docker exec $docker_id sh -c "/opt/ViperMonkey/vipermonkey/vmonkey.py -s --ioc '/root/$file_basename' -o /root/report.json"
-sudo docker cp "$docker_id:/root/report.json" "$2"
+docker exec $docker_id sh -c "/opt/ViperMonkey/vipermonkey/vmonkey.py -s --ioc '/root/$file_basename' -o /root/report.json"
+docker cp "$docker_id:/root/report.json" "$2"
 echo "[*] json report saved to '$2'"
 else
 echo "[!] Please supply at least 1 argument to analyze a file. Supply a 2nd argument for json file output. No more than 2 arguments are supported."
 fi
 
 echo "[*] Done - Killing docker container $docker_id"
-sudo docker stop $docker_id > /dev/null
+docker stop $docker_id > /dev/null
 
